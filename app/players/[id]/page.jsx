f530ef60a3bddd32dba145fa95b11c88638b3514
DanielBlz3 <gunnerxeditz@gmail.com>
2025-05-28 17:52:12 -0400

    your commit message

--- app/players/[id]/page.jsx
+++ app/players/[id]/page.jsx
@@ -1,12 +1,12 @@
 // app/players/[id]/page.jsx
 import PlayerPageClient from './PlayerPageClient';
 async function getPlayerData(id) {
-  const res = await fetch(`http://localhost:3000/api/players/${id}`, {
-    cache: 'no-store'
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/players/${id}`, {
+    cache: 'no-store',
   });
 
-  if (!res.ok) throw new Error('Failed to fetch player');
-  return res.json();
+if (!res.ok) throw new Error('Failed to fetch player');
+return res.json();
 }
 
 export default async function PlayerPage({ params }) {
