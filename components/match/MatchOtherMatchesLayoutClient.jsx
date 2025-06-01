4bc8f7b3eef8cc4a0b0dc72486ccadab224c1cbd
DanielBlz3 <gunnerxeditz@gmail.com>
2025-06-01 02:56:08 -0400

    your commit message

--- components/match/MatchOtherMatchesLayoutClient.jsx
+++ components/match/MatchOtherMatchesLayoutClient.jsx
@@ -9,28 +9,60 @@ import { translationsMap } from '/lib/translations';
 import formatMatchTimestamp from '/lib/timeFormatter';
 import '/styles/global.css';
 
-export default function Layout({ otherFixtures, children }) {
+export default function Layout({ otherFixtures, match, children }) {
 
-    const matchContainer = css`
-height: 3rem;
-width: 100%;
+    const [language, setLanguage] = useState('es');
+    useEffect(() => {
+        const storedlanguage = localStorage.getItem('language') || 'es';
+        setLanguage(storedlanguage)
+    })
+
+
+    const otherMatches = css`
+    text-decoration: none;
+      background-color: var(--card-bg-main);
+      width: 20rem;
+      border-radius: 1.25rem;
+      padding: 1rem;
+    `;
+    const otherMatchesHeader = css`
+    display: flex;
+    flex-flow: row;
+    gap: 10px;
+    height: 4rem;
+    place-items: center
+    `;
+
+    const otherMatchesMain = css`
+`;;
+
+    const matchLink = css`
+color: var(--primary-font-color);
+text-decoration: none;
 `;
 
     const matchWrapper = css`
 display: flex;
 flex-flow: row;
 place-items: center;
+height: 8rem;
+     border-top: 1px solid var(--divider-bg-primary);
 `;
 
     const matchInfo = css`
     display: flex;
     flex-flow: column;
-    flex: 2;
+    flex: 3;
     place-items: center;
     justify-content: center;
+    gap: 12px;
+        width: 100%;
+             border-right: 1px solid var(--divider-bg-primary);
+
 `;
 
     const matchStatus = css`
+    flex: 1;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -40,6 +72,7 @@ place-items: center;
     display: flex;
     flex-flow: row;
     align-items: center;
+    width: 100%;
 `;
 
     const matchTeamContent = css`
@@ -51,17 +84,24 @@ place-items: center;
 `
 
     const matchTeamScore = css`
-    justify-self: end;
-        flex: 1;
+    display: flex;
+    justify-content: center;
+    flex: 2;
+`;
+
+    const leagueText = css`
+display: flex;
+flex-flow: column;
 `
 
-
-    const [language, setLanguage] = useState('es');
+const fixturesRound = css`
+color: rgb(128, 128, 128);
+`
 
     const RenderOtherFixtures = () => {
         const otherFixturesEl = otherFixtures.map(m => {
             return (
-                <div css={matchContainer}>
+                <a css={matchLink} href={m.pageUrl}>
                     <div css={matchWrapper}>
                         <div css={matchInfo}>
                             <div css={matchTeam}>
@@ -87,11 +127,23 @@ place-items: center;
                             <span>FT</span>
                         </div>
                     </div>
-                </div>
+                </a>
             )
         })
+        console.log(match.general.leagueLogo)
         return (
-            otherFixturesEl
+            <div css={otherMatches}>
+                <header css={otherMatchesHeader}>
+                    <img src={match.general.leagueLogo} width={25} height={25} />
+                    <div css={leagueText}>
+                        <h3>{match.general.leagueName}</h3>
+                        <span css={fixturesRound}>{`${translationsMap?.["round"]?.[language]} ${match.general.roundName}`}</span>
+                    </div>
+                </header>
+                <div css={otherMatchesMain}>
+                    {otherFixturesEl}
+                </div>
+            </div>
         )
     }
 
